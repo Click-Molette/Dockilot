b6454b91a89135fce5f2449af3c0c60b94b71144
Tacx <12997062+tacxou@users.noreply.github.com>
2024-06-27 20:16:33 +0200

    feat: Add ContainerUpdateOptions interface to shared-types package

---
+++ packages/shared-types/src/container-update.options.ts
@@ -0,0 +1,50 @@
+interface ThrottleDevice {
+  Path: string
+  Rate: number
+}
+
+export interface ContainerUpdateOptions {
+  CpuShares: number
+  Memory: number
+  CgroupParent: string
+  BlkioWeight: number
+  BlkioWeightDevice: { Path: string, Weight: number }[]
+  BlkioDeviceReadBps: ThrottleDevice[]
+  BlkioDeviceWriteBps: ThrottleDevice[]
+  BlkioDeviceReadIOps: ThrottleDevice[]
+  BlkioDeviceWriteIOps: ThrottleDevice[]
+  CpuPeriod: number
+  CpuQuota: number
+  CpuRealtimePeriod: number
+  CpuRealtimeRuntime: number
+  CpusetCpus: string
+  CpusetMems: string
+  Devices: {
+    PathOnHost: string
+    PathInContainer: string
+    CgroupPermissions: string
+  }[]
+  DeviceCgroupRules: string[]
+  DiskQuota: number
+  KernelMemory: number
+  MemoryReservation: number
+  MemorySwap: number
+  MemorySwappiness: number
+  NanoCpus: number
+  OomKillDisable: boolean
+  Init: boolean | null
+  PidsLimit: number
+  Ulimits: {
+    Name: string
+    Soft: number
+    Hard: number
+  }[]
+  CpuCount: number
+  CpuPercent: number
+  IOMaximumIOps: number
+  IOMaximumBandwidth: number
+  RestartPolicy: {
+    Name: string
+    MaximumRetryCount?: number
+  }
+}
