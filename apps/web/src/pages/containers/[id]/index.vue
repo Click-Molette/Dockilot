2da3aba61bc04c1ab0a8036318fd19f1c8fb2d35
Tacx <12997062+tacxou@users.noreply.github.com>
2024-09-30 18:39:00 +0200

    refactor: Update container index.vue to include custom components for volumes, labels, and environments

--- apps/web/src/pages/containers/[id]/index.vue
+++ apps/web/src/pages/containers/[id]/index.vue
@@ -1,4 +1,8 @@
 <template lang="pug">
+  q-custom-common-volumes(:volumes='props.container?.Config?.Volumes')
+  q-custom-common-labels(:labels='props.container.Config.Labels')
+  q-custom-common-labels(:labels='props.container.Config.Labels')
+  q-custom-common-environments(:environments='props.container.Config.Env')
   q-expansion-item.bg-blue-grey-10.full-width(v-if='debug' label='Debug' icon='mdi-bug' dark dense)
     q-card.overflow-auto.bg-blue-grey-8.text-white(:style='{maxHeight: "250px", height: "250px"}')
       q-card-section.q-pa-xs
